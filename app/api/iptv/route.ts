88a572bc432b4883a25c01e4d376a57ca0f9b0c9
wxjwsw4545 <tinajanice6745@gmail.com>
2026-02-18 21:21:07 +0800

    Update route.ts

--- app/api/iptv/route.ts
+++ app/api/iptv/route.ts
@@ -4,7 +4,7 @@
  */
 
 import { NextRequest, NextResponse } from 'next/server';
-
+export const runtime = 'edge';
 export const runtime = 'nodejs';
 
 export async function GET(request: NextRequest) {
